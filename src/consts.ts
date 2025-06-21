f09dc4015446029b9990942a89f4923ae486c3d5
DeWa <DeWa@users.noreply.github.com>
2025-06-21 11:06:29 +0000

    deploy: f96d79064c4cddb631ae333a0e864d1b8b0af94c

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-import { parseColor } from "@chakra-ui/react";
-import { ToolType } from "./types";
-
-export const DEFAULT_TOOLBAR_PROPS = {
-  selectedChar: " ",
-  selectedCharColor: parseColor("#ffffff"),
-  selectedBgColor: parseColor("#000000"),
-  selectedTool: ToolType.Brush,
-};
